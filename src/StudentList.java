1db99218fd2f44ac6374649e016bb1ee9bfa0ba5
shreeharibadiger <shreeharibadiger@gmail.com>
2026-02-04 10:53:36 +0530

    hi

---
+++ src/StudentList.java
@@ -0,0 +1,17 @@
+import java.nio.charset.CharacterCodingException;
+import java.util.ArrayList;
+
+public class StudentList {
+    public static  void main(String[] args) {
+        ArrayList<String> stname= new ArrayList<>();
+        stname.add("shreehari");
+        stname.add("praveen");
+        stname.add("chetan");
+        stname.add("maruf");
+        stname.add("sufiyan");
+        stname.add("pramod");;
+        System.out.println(stname);
+
+
+    }
+}
